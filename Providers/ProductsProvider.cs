f644cab0b18ad01aa343c444837bccd5f4a975f6
Gourav Goel <gourav1787@gmail.com>
2022-09-20 23:22:18 +0530

    Add project files.

---
+++ Providers/ProductsProvider.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using ECommerce.Api.Products.Db;
+using ECommerce.Api.Products.Interfaces;
+using ECommerce.Api.Products.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.Providers
+{
+    public class ProductsProvider : IProductsProvider
+    {
+        private readonly ProductsDbContext dbContext;
+        private readonly IMapper mapper;
+        private readonly ILogger<ProductsProvider> logger;
+
+        public ProductsProvider(ProductsDbContext dbContext, ILogger<ProductsProvider> logger
+            , IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+            this.mapper = mapper;
+
+            SeedData();
+        }
+
+        private void SeedData()
+        {
+            if (!dbContext.Products.Any())
+            {
+                dbContext.Products.Add(new Db.Product() { Id = 1, Name = "Keyboard", Price = 100, Inventory = 2321 });
+                dbContext.Products.Add(new Db.Product() { Id = 2, Name = "Mouse", Price = 200, Inventory = 2322 });
+                dbContext.Products.Add(new Db.Product() { Id = 3, Name = "CPU", Price = 300, Inventory = 2323 });
+                dbContext.Products.Add(new Db.Product() { Id = 4, Name = "Monitor", Price = 400, Inventory = 2324 });
+                dbContext.SaveChanges();
+            }
+        }
+
+        public async Task<(bool IsSuccess, IEnumerable<Models.Product> Products,
+            string ErrorMessage)> GetProductsAsync()
+        {
+            try
+            {
+                var products = await dbContext.Products.ToListAsync();
+                if (!(products == null && !products.Any()))
+                {
+                    var result = mapper.Map<IEnumerable<Db.Product>, IEnumerable<Models.Product>>(products);
+                    return (true, result, null);
+                }
+                return (false, null, "Not Found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+        public async Task<(bool IsSuccess, Models.Product Product,
+            string ErrorMessage)> GetProductAsync(int id)
+        {
+            try
+            {
+                var product = await dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+                if (product != null)
+                {
+                    var result = mapper.Map<Db.Product, Models.Product>(product);
+                    return (true, result, null);
+                }
+                return (false, null, "Not Found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+    }
+}
